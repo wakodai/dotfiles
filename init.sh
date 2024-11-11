9e5d7206e440083a0f8d8ec74b74fb440635f601
若生大輔 <wakodai@wakoudaisukenoMacBook-Pro.local>
2024-11-12 00:35:22 +0900

    init.sh

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$(uname)" != "Darwin" ] ; then
+	echo "Not macOS!"
+	exit 1
+fi
+
+# Install xcode
+#xcode-select --install > /dev/null
+# TODO: すでにインストール済みならスキップするようにしたい
+
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
